f7b0da92afca2280cd2512b7328ed0238bedf86c
Tina Paunovic <tina.paunovic@fonis.rs>
2023-12-21 19:56:28 +0100

    Napravljen svi kontorlori

--- e-library/app/Http/Controllers/BookController.php
+++ e-library/app/Http/Controllers/BookController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\AuthorResources\AuthorCollection;
-use App\Http\Resources\AuthorResources\AuthorResource;
-use App\Models\Author;
+use App\Http\Resources\BookResources\BookCollection;
+use App\Http\Resources\BookResources\BookResource;
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class AuthorController extends Controller
+
+class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -17,9 +18,10 @@ class AuthorController extends Controller
      */
     public function index()
     {
-        $authors = Author::all();
-        return new AuthorCollection($authors);
+        $books = Book::all();
+        return new BookCollection($books);
     }
+
 
     /**
      * Store a newly created resource in storage.
@@ -31,74 +33,79 @@ class AuthorController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:50',
-            'surname' => 'required|string|max:50'
+            'num_pages' => 'required',
+            'author_id' => 'required',
+            'genre_id' => 'required'
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors());
         }
 
-        $author = Author::create([
+        $book = Book::create([
             'name' => $request->name,
-            'surname' => $request->surname
+            'num_pages' => $request->num_pages,
+            'author_id' => $request->author_id,
+            'genre_id' => $request->genre_id,
+            
         ]);
 
-        return response()->json(['Author created successfully.', new AuthorResource($author), 'success' => true]);
+        return response()->json(['Book created successfully.', new BookResource($book), 'success' => true]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Author  $author
+     * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function show($author_id)
+    public function show(Book $book)
     {
-        $author = Author::find($author_id);
-        if (is_null($author)) {
-            return response()->json('Data not found', 404);
-        }
-        return new AuthorResource($author);
+        return new BookResource($book);
     }
-
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Author  $author
+     * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        $author = Author::find($id);
+        $book = Book::find($id);
 
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:50',
-            'surname' => 'required|string|max:50'
+            'num_pages' => 'required',
+            'author_id' => 'required',
+            'genre_id' => 'required'
         ]);
 
         if ($validator->fails())
             return response()->json($validator->errors());
 
 
-        $author->name = $request->name;
-        $author->surname = $request->surname;
+        $book->name = $request->name;
+        $book->num_pages = $request->num_pages;
+        $book->author_id = $request->author_id;
+        $book->genre_id = $request->genre_id;
 
-        $author->save();
 
-        return response()->json(['Author updated successfully.', new AuthorResource($author), 'success' => true]);
+        $book->save();
+
+        return response()->json(['Book updated successfully.', new BookResource($book), 'success' => true]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Author  $author
+     * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Author $author)
+    public function destroy(Book $book)
     {
-        $author->delete();
-        return response()->json('Author deleted successfully.');
+        $book->delete();
+        return response()->json('Book deleted successfully.');
     }
 }
---
+++ e-library/app/Http/Controllers/FavBookController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\BookResources\FavBookCollection;
+use App\Http\Resources\BookResources\FavBookResource;
+use App\Models\FavBook;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class FavBookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user_id = auth()->user()->id;
+        //$user = $request->user;
+        //$user_id = $user->id;
+        $books = FavBook::get()->where('user_id', $user_id);
+        if (is_null($books)) {
+            return response()->json('Data not found', 404);
+        }
+        return new FavBookCollection($books);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'book_id' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $favBook = FavBook::create([
+            'book_id' => $request->book_id,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return response()->json(['Favourite book created successfully.', new FavBookResource($favBook), 'success' => true]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FavBook  $favBook
+     * @return \Illuminate\Http\Response
+     */
+    public function show($favBook_id)
+    {
+
+
+        $favBook = FavBook::get()->where('id', $favBook_id)->first();
+        if (!$favBook) {
+            return response()->json('Data not found', 404);
+        }
+        return new FavBookResource($favBook);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($favBook_id)
+    {
+
+        $favBook = FavBook::get()->where('id', $favBook_id)->first();
+        if (!$favBook) {
+            return response()->json('Data not found', 404);
+        }
+
+        $favBook->delete();
+
+        return response()->json('Book removed from favourites successfully.');
+    }
+}
