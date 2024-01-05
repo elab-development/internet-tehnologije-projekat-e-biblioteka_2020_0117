fff28af34ea61adaaed2802f21684dc2f38676dc
Tina Paunovic <tina.paunovic@fonis.rs>
2024-01-05 21:40:48 +0100

    Dodato search dugme za navbar

--- react/frontend/src/components/NavBar.jsx
+++ react/frontend/src/components/NavBar.jsx
@@ -11,13 +11,23 @@ const NavBar = () => {
   return (
     <div>
         
-    <div class="nav">
+    <div className="nav">
         <ul>
             <li><a href="">Books</a></li>
             <li><a href="">Profile</a></li>
             <li><a href="">Contact</a></li>
             <li><a href="">Payment</a></li>
             <li><a href="">Logout</a></li>
+            <li className="nav-item search-icon">
+               <button className="search-button"><i className="fas fa-search">Search</i></button>
+            </li>
+            <li className="nav-item search-icon">
+          <input
+            type="text"
+            placeholder="Unesite tekst"
+            className="search-input"
+          />
+        </li>
         </ul>
     </div>
     </div>
