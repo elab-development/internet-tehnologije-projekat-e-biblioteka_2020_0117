fa42d6dcf292278cf14504a33993c6c414be46d3
Tina Paunovic <tina.paunovic@fonis.rs>
2024-01-15 00:30:07 +0100

    Ispravka mape

--- react/frontend/src/components/Contact.jsx
+++ react/frontend/src/components/Contact.jsx
@@ -2,6 +2,7 @@ import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { useMemo, useState, useEffect } from "react";
 import axios from 'axios';
 import "../style/Contact.css";
+import MapComponent from './MapComponent';
 
 const Citat = () => {
   const [citat, setCitat] = useState('');
@@ -9,7 +10,9 @@ const Citat = () => {
   useEffect(() => {
     const povuciCitat = async () => {
       try {
-        const odgovor = await axios.get('https://api.quotable.io/random');
+       const odgovor = await axios.get('https://api.quotable.io/random');
+      
+        
         
        
         if (odgovor.data && odgovor.data.content) {
@@ -34,16 +37,19 @@ const Citat = () => {
   );
 };
 
+
+
+
 const Contact = () => {
-  const { isLoaded } = useLoadScript({
+  /*const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);  */
 
   return (
     <div>
        <Citat />
-      <div className="App">
+      {/*<div className="App">
         {!isLoaded ? (
           <h1>Loading...</h1>
         ) : (
@@ -55,12 +61,15 @@ const Contact = () => {
             <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
           </GoogleMap>
         )}
-      </div>
+        </div>  */}
+        <MapComponent />
 
      
     </div>
   );
-};
+};  
+
+
 
 export default Contact;
 
